e02ef75e917718b8ca8fd9573bf0d6f0f5035cd2
Hiroyuki Komatsuzawa <kohiandrew@gmail.com>
2019-04-05 01:22:13 +0900

    Mod content dir

--- src/pages/ja/index.tsx
+++ src/pages/ja/index.tsx
@@ -1,9 +1,9 @@
 import React from "react"
 import { Link, graphql } from "gatsby"
 
-import Bio from "../components/bio"
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Bio from "../../components/bio"
+import Layout from "../../components/layout"
+import SEO from "../../components/seo"
 
 class BlogIndex extends React.Component<any, any> {
   render() {
